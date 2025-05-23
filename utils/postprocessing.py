2527913769c452f92b140cf3501d1ff8ae714979
Alaa Elkaffas <120778210+alaa-alt@users.noreply.github.com>
2025-05-23 18:24:50 +0300

    Delete utils directory

--- utils/postprocessing.py
+++
@@ -1,9 +0,0 @@
-from skimage.measure import label
-
-def count_fragments(mask):
-    """Count number of connected components in binary mask"""
-    return label(mask).max()
-
-def calculate_area(mask):
-    """Returns area in pixels"""
-    return int(mask.sum())
